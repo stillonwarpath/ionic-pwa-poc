6d7d96b9a07254c16cbc67c0a9cb8795902283f6
francisco <fr.betancourt@live.com>
2022-02-23 20:16:01 -0300

    Removed support for some ios safari and safari versions because building was failing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Geolocation, Position } from '@capacitor/geolocation';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  myImage: string = null;
+  position: Position = null;
+
   constructor() {}
+
+  async takePicture() {
+
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera
+    });
+
+    this.myImage = image.webPath;
+
+  }
+
+  async getCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.position = coordinates;
+  }
+
+  async share() {
+    await Share.share({
+      title: 'Come and find me',
+      text: `Here is my current location:
+          ${this.position.coords.latitude},
+          ${this.position.coords.longitude}`,
+      url: 'http://ionicacademy.com/'
+    })
+  }
 
 }
